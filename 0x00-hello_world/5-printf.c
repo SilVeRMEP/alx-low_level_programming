d56be4be033cd0fb8deaf3d1ed7083960489d42f
SilVeRMEP <abdousami855@gmail.com>
2023-03-15 23:15:10 -0700

    2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,2 @@
-#!/bin/bash
-echo '#include <stdio.h>' > program.c && echo 'int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return 0; }' >> program.c && gcc -Wall program.c -o program && ./program
-
+#include <stdio.h>
+int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return 0; }
