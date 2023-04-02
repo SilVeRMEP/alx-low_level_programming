381a2820c3ea091eb27d5f21e90510134c54ca98
SilVeRMEP <123823015+SilVeRMEP@users.noreply.github.com>
2023-04-02 01:37:34 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 
 /**
- *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
- *
- *Return: Always 0 (Success)
- */
+*main - print all two-digit numbers with no repeated digits
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	int ones;
-	int tens;
+int tens_digit;
+int ones_digit;
+int print_comma = 0; 
 
-	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
-	{
-		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
-		{
-			putchar(tens);
-			putchar(ones);
+scss
+Copy code
+for (tens_digit = 0; tens_digit < 10; tens_digit++) 
+{
+    for (ones_digit = 0; ones_digit < 10; ones_digit++) 
+    {
+        if (tens_digit != ones_digit)
+        {
+            putchar(tens_digit + '0');
+            putchar(ones_digit + '0');
 
-			if (tens != '8' || ones != '9') /*print commas*/
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
+            
+            if (!(tens_digit == 9 && ones_digit == 9))
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+}
 
-	return (0);
+putchar('\n');
+
+return (0);
 }
