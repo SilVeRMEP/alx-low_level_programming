6f4ceec725949cacaaf5ea40babe136d7d0d95ed
SilVeRMEP <123823015+SilVeRMEP@users.noreply.github.com>
2023-04-02 11:24:00 +0000

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
- *main - print alphabet reversed using putchar
- *Return: Always 0 (Success)
+ * main - Prints the lowercase alphabet in reverse order using putchar.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	char alpha = 'z';
+    char letter;
 
-	while (alpha >= 'a')
-	{
-		putchar(alpha);
-		alpha--;
-	}
-	putchar('\n');
+    for (letter = 'z'; letter >= 'a'; letter--)
+    {
+        putchar(letter);
+    }
 
-	return (0);
+    putchar('\n');
+    return 0;
 }
