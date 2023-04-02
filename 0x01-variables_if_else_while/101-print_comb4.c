70c32c9a9cbc809ffa805f34a546aa5f9a3cd311
SilVeRMEP <123823015+SilVeRMEP@users.noreply.github.com>
2023-04-02 01:40:09 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 /**
- *main - print all combinations of three different digits
- *Return: Always 0 (Success)
+ * main - Prints all combinations of three different digits
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int one;
-	int ten;
-	int hundred;
+	int digit_one;
+	int digit_ten;
+	int digit_hundred;
 
-	for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
+	for (digit_hundred = '0'; digit_hundred <= '9'; digit_hundred++) /* Hundreds place */
 	{
-		for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
+		for (digit_ten = (digit_hundred + 1); digit_ten <= '9'; digit_ten++) /* Tens = Hundreds + 1 */
 		{
-			for (one = (ten + 1); one <= '9'; one++) /*ones*/
+			for (digit_one = (digit_ten + 1); digit_one <= '9'; digit_one++) /* Ones */
 			{
-				putchar(hundred);
-				putchar(ten);
-				putchar(one);
-				if (hundred != '7' || ten != '8' || one != '9')
+				putchar(digit_hundred);
+				putchar(digit_ten);
+				putchar(digit_one);
+				if (digit_hundred != '7' || digit_ten != '8' || digit_one != '9')
 				{
 					putchar(',');
 					putchar(' ');
