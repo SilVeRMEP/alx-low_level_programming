e137dbe1ff1914eaad3cb9604e3f1769dbae2e7d
SilVeRMEP <123823015+SilVeRMEP@users.noreply.github.com>
2023-04-02 12:19:36 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,50 +6,24 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    int tens1, ones1, tens2, ones2, tens3, ones3;
-
-    for (tens1 = 0; tens1 <= 9; tens1++)
-    {
-        for (ones1 = 0; ones1 <= 9; ones1++)
-        {
-            for (tens2 = tens1; tens2 <= 9; tens2++)
-            {
-                int start = (tens1 == tens2) ? ones1 + 1 : 0;
-
-                for (ones2 = start; ones2 <= 9; ones2++)
-                {
-                    for (tens3 = tens2; tens3 <= 9; tens3++)
-                    {
-                        int start2 = (tens2 == tens3) ? ones2 + 1 : 0;
-
-                        for (ones3 = start2; ones3 <= 9; ones3++)
-                        {
-                            putchar(tens1 + '0');
-                            putchar(ones1 + '0');
-                            putchar(' ');
-                            putchar(tens2 + '0');
-                            putchar(ones2 + '0');
-                            putchar(' ');
-                            putchar(tens3 + '0');
-                            putchar(ones3 + '0');
-
-                            if (tens1 == 9 && ones1 == 8 && tens2 == 9 && ones2 == 9 && tens3 == 9 && ones3 == 9)
-                            {
-                                putchar('\n');
-                                break;
-                            }
-
-                            putchar(',');
-                            putchar(' ');
-                        }
-                    }
-                }
+int main() {
+    int i = 0, j = 0;
+    while (i <= 99) {
+        j = i + 1;
+        while (j <= 99) {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (i != 98 || j != 99) {
+                putchar(',');
+                putchar(' ');
             }
+            j++;
         }
+        i++;
     }
-
-    return (0);
+    return 0;
 }
 
