2ecdad0e8260d9454a3902aadd4f92b462eb134e
SilVeRMEP <123823015+SilVeRMEP@users.noreply.github.com>
2023-04-02 11:48:05 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,11 @@
 int main(void)
 {
     int n;
+
     srand(time(0));
+
     n = rand() - RAND_MAX / 2;
+
     if (n > 0)
     {
         printf("%i is positive\n", n);
@@ -23,5 +26,6 @@ int main(void)
     {
         printf("%i is negative\n", n);
     }
+
     return (0);
 }
