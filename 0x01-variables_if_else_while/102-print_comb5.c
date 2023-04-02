0ed345c9e5cfba0da81f4013e635ba0907a07060
SilVeRMEP <123823015+SilVeRMEP@users.noreply.github.com>
2023-04-02 14:00:07 +0000

    ds
    sd

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,30 +5,44 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void	put(int n)
 {
-  int i;
-  int j;
+	if (n < 10)
+	{
+		putchar('0');
+		putchar('0' + n);
+	}
+	else
+	{
+		putchar('0' + n / 10);
+		putchar('0' + n % 10);
+	}
+}
+int	main(void)
+{
+	int i;
+	int	j;
 
-  i = 0;
-  while (i < 10)
-  {
-    j = i + 1;
-    while (j < 10)
-    {
-      putchar('0' + i);
-      putchar('0' + j);
-      if (i != 8 || j != 9)
-      {
-        putchar(',');
-        putchar(' ');
-       }
-     j++;
-     }
-    i++;
-    }
-  putchar('\n');
-  return (0);
+	i = 0;
+	while (i < 100)
+	{
+		j = i + 1;
+		while (j < 100)
+		{
+			put(i);
+			putchar(' ');
+			put(j);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
 
 
