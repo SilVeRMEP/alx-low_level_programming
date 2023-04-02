f144924625c3aaa36fc6499a03b548311ebdf83f
SilVeRMEP <123823015+SilVeRMEP@users.noreply.github.com>
2023-04-02 11:54:52 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 
 /**
-*main - print all two-digit numbers with no repeated digits
-*Return: Always 0 (Success)
-*/
-
+ * main - Prints all possible combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int tens_digit;
-int ones_digit;
-int print_comma = 0; 
-for (tens_digit = 0; tens_digit < 10; tens_digit++) 
-{
-    for (ones_digit = 0; ones_digit < 10; ones_digit++) 
-    {
-        if (tens_digit != ones_digit)
-        {
-            putchar(tens_digit + '0');
-            putchar(ones_digit + '0');
+	int i, j;
 
-            
-            if (!(tens_digit == 9 && ones_digit == 9))
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-}
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			if (i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
 
-putchar('\n');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-return (0);
+	putchar('\n');
+
+	return (0);
 }
