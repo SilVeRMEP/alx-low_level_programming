796f52cdaa461fa631ace24c416357d10fd22a2a
SilVeRMEP <123823015+SilVeRMEP@users.noreply.github.com>
2023-04-02 13:34:22 +0000

    11
    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+ #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
@@ -13,11 +13,11 @@ int main(void)
 	int digit_ten;
 	int digit_hundred;
 
-	for (digit_hundred = '0'; digit_hundred <= '9'; digit_hundred++) /* Hundreds place */
+	for (digit_hundred = '0'; digit_hundred <= '9'; digit_hundred++) 
 	{
-		for (digit_ten = (digit_hundred + 1); digit_ten <= '9'; digit_ten++) /* Tens = Hundreds + 1 */
+		for (digit_ten = (digit_hundred + 1); digit_ten <= '9'; digit_ten++) 
 		{
-			for (digit_one = (digit_ten + 1); digit_one <= '9'; digit_one++) /* Ones */
+			for (digit_one = (digit_ten + 1); digit_one <= '9'; digit_one++) 
 			{
 				putchar(digit_hundred);
 				putchar(digit_ten);
