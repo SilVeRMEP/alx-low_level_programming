bc88ef18f59c22b02b96fa68b8f2f84d8bc458a8
SilVeRMEP <abdousami855@gmail.com>
2023-04-03 13:26:41 -0700

    4

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * @s: The string to search.
  * @c: The character to locate.
  *
- * Return: A pointer to the first occurrence of the character c in the string s,
+ * Return: The character c in the string s,
  *         or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
