3f76f447f3c77cb3efc363fbabcd665a31e14350
SilVeRMEP <abdousami855@gmail.com>
2023-04-05 12:52:54 -0700

    223

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of a string
+ * _print_rev_recursion - Prints a string in reverse
  *
- * @s: String
+ * @s: Pointer to a string
  *
- * Return: Length of s
+ * Return: void
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-        return 0;
+	if (*s == '\0')
+	{
+		return;
+	}
 
-    return 1 + _strlen_recursion(s + 1);
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,48 +2,33 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to find square root of
+ * @n: the number to find the square root of
  *
- * Return: the natural square root of n, or -1 if n does not have a natural
- * square root
+ * Return: the natural square root of n, or -1 if it doesn't exist
  */
 int _sqrt_recursion(int n)
 {
-    return (sqrt_helper(1, n / 2, n));
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		return (find_sqrt(n, 1));
 }
 
 /**
- * sqrt_helper - recursively finds square root of n
- * @left: left endpoint of binary search interval
- * @right: right endpoint of binary search interval
- * @n: number to find square root of
+ * find_sqrt - helper function to recursively find the square root
+ * @n: the number to find the square root of
+ * @i: the integer to start checking for the square root
  *
- * Return: square root of n, or -1 if n does not have a natural square root
+ * Return: the square root of n, or -1 if it doesn't exist
  */
-int sqrt_helper(int left, int right, int n)
+int find_sqrt(int n, int i)
 {
-    int mid;
-
-    if (n < 0)
-        return (-1);
-    if (n == 0 || n == 1)
-        return (n);
-
-    mid = (left + right) / 2;
-    if (mid * mid == n)
-        return (mid);
-    else if (mid * mid < n)
-    {
-        if (mid + 1 <= right)
-            return (sqrt_helper(mid + 1, right, n));
-        else
-            return (-1);
-    }
-    else
-    {
-        if (left <= mid - 1)
-            return (sqrt_helper(left, mid - 1, n));
-        else
-            return (-1);
-    }
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	else
+		return (find_sqrt(n, i + 1));
 }
