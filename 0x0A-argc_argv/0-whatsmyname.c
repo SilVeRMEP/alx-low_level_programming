f2ef0fc9883a2e814044304c8ef7b87f809427d7
SilVeRMEP <abdousami855@gmail.com>
2023-04-09 17:31:22 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: 0 on success, 1 if no program name is provided
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+	        _putchar("%s\n", argv[0]);
+		return (0);
+	}
+	else
+	{
+		_putchar("Error: No program name provided.\n");
+		return (1);
+	}
+}
