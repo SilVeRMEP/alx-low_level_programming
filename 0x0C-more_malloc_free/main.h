48d1fd651d088732962a78bf2b280382ee05e6fe
SilVeRMEP <abdousami855@gmail.com>
2023-04-13 16:14:33 -0700

    3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,6 +12,10 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+void print_error_and_exit(void);
+int is_valid_number(char *num);
+int multiply(int num1, int num2);
+int main(int argc, char *argv[]);
 
 
 #endif
