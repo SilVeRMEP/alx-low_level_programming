fec87bcad77a80a2b5d544fc2bf0de7b8bd309f2
SilVeRMEP <abdousami855@gmail.com>
2023-04-17 16:00:47 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,31 +1,17 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
- * print_dog - Prints the details of a struct dog variable
- * @d: Pointer to the struct dog variable to be printed
+ * print_dog - Prints the contents of a struct dog.
+ * @d: Pointer to a struct dog representing the dog to be printed.
+ *
+ * Return: None.
  */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
 
-	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 	printf("Age: %.1f\n", d->age);
-	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
-}
-
-int main(void)
-{
-	struct dog my_dog;
-
-	my_dog.name = "Buddy";
-	my_dog.age = 3.5;
-	my_dog.owner = "John";
-
-	print_dog(&my_dog);
-
-	return (0);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+/**
+ * new_dog - Creates a new struct dog and initializes its members.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner:  the owner of the dog.
+ *
+ * Return: Pointer , or NULL if memory allocation fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+	char *newName, *newOwner;
+	int nameLen, ownerLen;
+
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
+
+	nameLen = strlen(name);
+	ownerLen = strlen(owner);
+
+	newName = malloc((nameLen + 1) * sizeof(char));
+	if (newName == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+
+	newOwner = malloc((ownerLen + 1) * sizeof(char));
+	if (newOwner == NULL)
+	{
+		free(newName);
+		free(newDog);
+		return (NULL);
+	}
+
+	strcpy(newName, name);
+	strcpy(newOwner, owner);
+
+	newDog->name = newName;
+	newDog->age = age;
+	newDog->owner = newOwner;
+
+	return (newDog);
+}
