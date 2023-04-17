b7f94f0ee1501c09fd2fb562ccbefc5cd7c28ffd
SilVeRMEP <abdousami855@gmail.com>
2023-04-17 15:37:07 -0700

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	struct dog my_dog;
+
 	init_dog(&my_dog, "Buddy", 3.5, "John");
 
 	printf("Dog's name: %s\n", my_dog.name);
 	printf("Dog's age: %.1f\n", my_dog.age);
 	printf("Dog's owner: %s\n", my_dog.owner);
 
-	return 0;
+	return (0);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -31,4 +31,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 
-#endif 
+#endif
