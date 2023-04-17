9e5825bb610d96b70261b890bca5cd3863708343
SilVeRMEP <abdousami855@gmail.com>
2023-04-17 15:55:34 -0700

    11111

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,31 +5,16 @@
  * @d: Pointer to the struct dog variable to be initialized
  * @name: Name of the dog
  * @age: Age of the dog
- * @owner: Owner of the dog
+ * @owner: Owner of the dog.
+ *
+ * Return: None.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL || name == NULL || owner == NULL)
-	{
-		printf("Error: Invalid input\n");
+	if (d == NULL)
 		return;
-	}
 
-	strncpy(d->name, name, sizeof(d->name) - 1);
-	d->name[sizeof(d->name) - 1] = '\0';
+	d->name = name;
 	d->age = age;
-	strncpy(d->owner, owner, sizeof(d->owner) - 1);
-	d->owner[sizeof(d->owner) - 1] = '\0';
-
-int main(void)
-{
-	struct dog my_dog;
-
-	init_dog(&my_dog, "Buddy", 3.5, "John");
-
-	printf("Name: %s\n", my_dog.name);
-	printf("Age: %.1f\n", my_dog.age);
-	printf("Owner: %s\n", my_dog.owner);
-
-	return (0);
+	d->owner = owner;
 }
