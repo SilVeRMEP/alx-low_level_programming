6a30cd31235dc06f0aed2ded529dfb02674917df
SilVeRMEP <abdousami855@gmail.com>
2023-04-17 16:19:08 -0700

    5

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,17 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %.2f\n", d->age);
-	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	FILE *stream = stdout;
+
+	if (d->name == NULL)
+		fprintf(stream, "Name: (nil)\n");
+	else
+		fprintf(stream, "Name: %s\n", d->name);
+
+	fprintf(stream, "Age: %.2f\n", d->age);
+
+	if (d->owner == NULL)
+		fprintf(stream, "Owner: (nil)\n");
+	else
+		fprintf(stream, "Owner: %s\n", d->owner);
 }
