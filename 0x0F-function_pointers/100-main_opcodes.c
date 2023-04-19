3f0a27ae576cdd713944cbb32057ea48d8075470
SilVeRMEP <abdousami855@gmail.com>
2023-04-19 14:27:58 -0700

    6

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,44 @@
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
-/**
- * main - Prints the opcodes of its own main function.
- * @argc: Number of arguments passed to the program.
- * @argv: Array of arguments passed to the program.
- *
- * Return: Always 0.
- */
+void print_opcodes(int num_bytes);
+
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		write(STDERR_FILENO, "Error\n", 6);
-		return (1);
+		exit(1);
 	}
 
-	int bytes = atoi(argv[1]);
+	int num_bytes = atoi(argv[1]);
 
-	if (bytes <= 0)
+	if (num_bytes <= 0)
 	{
-		write(STDERR_FILENO, "Error\n", 6);
-		return (2);
+		exit(2);
 	}
 
-	unsigned char *ptr = (unsigned char *)&main;
+	print_opcodes(num_bytes);
 
-	for (int i = 0; i < bytes; i++)
-	{
-		char hex[2];
-
-		sprintf(hex, "%02x", *(ptr + i));
-		write(STDOUT_FILENO, hex, 2);
-	}
-
-	write(STDOUT_FILENO, "\n", 1);
 	return (0);
+}
+
+void print_opcodes(int num_bytes)
+{
+	unsigned char *opcodes = (unsigned char *) malloc(num_bytes);
+
+	memset(opcodes, 0, num_bytes);
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		opcodes[i] = ((unsigned char *) print_opcodes)[i];
+	}
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", opcodes[i]);
+	}
+
+	printf("\n");
+	free(opcodes);
+	exit(0);
 }
