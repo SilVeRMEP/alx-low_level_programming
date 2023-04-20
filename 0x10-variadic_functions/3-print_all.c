2127f5bf02cac18a4a8617d9bc5f7344bf6e9bb9
SilVeRMEP <abdousami855@gmail.com>
2023-04-20 16:04:20 -0700

    33

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,103 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- * c: char
- * i: integer
- * f: float
- * s: char * (if the string is NULL, print (nil) instead)
- * any other char should be ignored
- * Return: void
+ * print_all - Prints anything based on a given format.
+ * @format: A list of types of arguments passed to the function.
+ *          c: char
+ *          i: integer
+ *          f: float
+ *          s: char *
+ *          Any other char should be ignored.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	char *str;
+	unsigned int i = 0, j;
+	char *separator = "";
+	print_format formats[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		switch (format[i])
+		j = 0;
+
+		while (formats[j].type)
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
+			if (formats[j].type == format[i])
+			{
+				printf("%s", separator);
+				formats[j].func(args);
+				separator = ", ";
 				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", (float) va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					printf("(nil)");
-				else
-					printf("%s", str);
-				break;
+			}
+			j++;
 		}
-		if (format[++i] && (
-					format[i - 1] == 'c' ||
-					format[i - 1] == 'i' ||
-					format[i - 1] == 'f' ||
-					format[i - 1] == 's'))
-			printf(", ");
+		i++;
 	}
+
 	printf("\n");
 	va_end(args);
+}
+
+/**
+ * print_char - Prints a char.
+ * @args: A list of arguments.
+ */
+void print_char(va_list args)
+{
+	int c = va_arg(args, int);
+
+	_putchar(c);
+}
+
+/**
+ * print_int - Prints an int.
+ * @args: A list of arguments.
+ */
+void print_int(va_list args)
+{
+	int n = va_arg(args, int);
+
+	print_number(n);
+}
+
+/**
+ * print_float - Prints a float.
+ * @args: A list of arguments.
+ */
+void print_float(va_list args)
+{
+	float f = (float)va_arg(args, double);
+
+	print_number((int)f);
+	_putchar('.');
+	if (f < 0)
+		f *= -1;
+	print_number((int)((f - (int)f) * 1000000));
+}
+
+/**
+ * print_string - Prints a string.
+ * @args: A list of arguments.
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
 }
