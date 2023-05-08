83401e59558fbdfc2b2644caa59cdd2d06e99cb7
SilVeRMEP <abdousami855@gmail.com>
2023-05-08 14:35:51 -0700

    0123

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: the actual number of letters it could read and print
+ * or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+		return (0);
+
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	buffer[n_read] = '\0';
+
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (n_written);
+}
